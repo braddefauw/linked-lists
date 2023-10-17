ff12bcc2b5959c5b8c47b36268ab2b026b13266d
Brad DeFauw <badefauw@gmail.com>
2023-10-17 09:45:46 -0500

    prepend(value) adds a new node containing value to the start of the list

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ function LinkedList(){
         }
     }
 
+    // prepend a new node containing a vlaue to the start of the linked list
+    this.prepend = function(value){
+        const newNode = new Node(value);
+        newNode.nextNode = this.head;
+        this.head = newNode;
+    }
+
     // display the linked list as an array
     this.toArray = function () {
         const result = [];
@@ -38,4 +45,6 @@ function LinkedList(){
 const linkedList = new LinkedList();
 linkedList.append(1);
 linkedList.append(2);
-console.log(linkedList.toArray()); 
+console.log(linkedList.toArray()); // Output: [1, 2]
+linkedList.prepend(0);
+console.log(linkedList.toArray()) // Output: [0, 1, 2]
