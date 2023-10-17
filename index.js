d3f3ef42e2356dcb81833bb3799a31433601c244
Brad DeFauw <badefauw@gmail.com>
2023-10-17 09:39:29 -0500

    append(value) adds a new node to the end of the list; display linked list as an array

--- index.js
+++ index.js
@@ -5,7 +5,37 @@ function Node(value){
 }
 
 // LinkedList class
-
 function LinkedList(){
-    this.head = null
+    this.head = null;
+
+    // Append a new node containing a value to the end of the linked list
+    this.append = function (value) {
+        const newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode
+        }else{
+            let current = this.head;
+            while(current.nextNode){
+                current = current.nextNode;
+            }
+            current.nextNode = newNode;
+        }
+    }
+
+    // display the linked list as an array
+    this.toArray = function () {
+        const result = [];
+        let current = this.head;
+        while (current) {
+            result.push(current.value);
+            current = current.nextNode;
+        }
+        return result
+    }
 }
+
+// example usage
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+console.log(linkedList.toArray()); 
